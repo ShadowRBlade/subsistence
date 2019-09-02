130c98e23229ce6387718bbc1396db4d94534f94
lx5321 <alexanderwebsite5321@gmail.com>
2019-09-02 13:07:09 +0530

    Initial Commit

---
+++ perks.py
@@ -0,0 +1,22 @@
+points = 100
+
+def strength(self):
+   print("strength") 
+
+def perception(self):
+    print("perception")
+
+def endurance(self):        
+    print("endurance")
+
+def charisma(self):
+    print("charisma")
+
+def intelligence(self):
+    print("intelligence")
+
+def agility(self):
+    print("agility")
+
+def luck(self):   
+    print("luck")
