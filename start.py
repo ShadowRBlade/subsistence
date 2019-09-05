b2cd2831283902ac5a721746267661411d1c208c
lx5321 <alexanderwebsite5321@gmail.com>
2019-09-05 12:01:38 +0530

    Saved State Retreival Added.

--- start.py
+++ start.py
@@ -1,12 +1,27 @@
 from time import sleep
 from sys import platform
+from os import system
+from os import path
 import perks
-
+import sqlite3
 
 platform = platform[:2]
 if (platform == 'li'):
     print("Running on Linux")
 elif(platform == 'wi'):
     print("Running on Windows")
+sleep(2)
 
-perks.setPerks()
+
+if(path.exists("gamedata.db")):
+    # then load saved state since db exists
+    print("Loading previous saved state.")
+else:
+    # create and write to db      
+    # create connection object    
+    conn = sqlite3.connect('gamedata.db')
+    # execute table creation if table does not exist
+    conn.execute('''CREATE TABLE IF NOT EXISTS PROGRESS (PROGRESSID INT PRIMARY KEY NOT NULL, PROGRESSLABEL TEXT NOT NULL, VALUE INT NOT NULL);''')
+    # set perks
+    perks.setPerks()
+    
