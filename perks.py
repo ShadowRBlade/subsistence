ff24bc8eda214f22327c27bf654573fb6536a4c6
lx5321 <alexanderwebsite5321@gmail.com>
2019-09-05 18:42:10 +0530

    General Code Refactor

--- perks.py
+++ perks.py
@@ -61,13 +61,13 @@ def setPerks():
     perkCollection.clear()
     initial = 18 # set to a value of 18
     print("******** Choose Your Perks ********")
-    print("\033[1;37;40m     18 Perk Points Available     \033[0;37;40m")
+    print("     18 Perk Points Available     ")
     strength(initial)
 
 def verify():
     # check if sum is 18 else call setPerks
     if(sum(perkCollection) != 18):
-        print("\033[1;31;40mInvalid Combination. Perks should amount to 18\033[0;37;40m")
+        print("Invalid Combination. Perks should amount to 18")
         input("Press any key to continue")
         setPerks()
 
